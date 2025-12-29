cdf07e97ae2dbcb519a663cde3d684e16e2bc7fd
Madhupa Minoli <mmadhupa@gmail.com>
2025-12-29 22:11:57 +0100

    Added small logic change to see incremental testing

--- src/converterCelsius.js
+++ src/converterCelsius.js
@@ -1,6 +1,6 @@
 class ConverterCelsius {
   convert(fahrenheit) {
-    return ((fahrenheit - 32) * 5) / 9; //adding a comment
+    return ((fahrenheit - 32) * 5) / 9 + 0.1; // small logic change
   }
 }
 
