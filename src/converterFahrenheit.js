556bc77ef4e6d46941ad83b1496161b154ae1691
Madhupa Minoli <mmadhupa@gmail.com>
2025-12-29 21:46:16 +0100

    Added null check for converterFahrenheit

--- src/converterFahrenheit.js
+++ src/converterFahrenheit.js
@@ -1,5 +1,9 @@
 class ConverterFahrenheit {
   convert(celsius) {
+    
+    if (celsius === null || celsius === undefined) {
+      throw new Error('Invalid input');
+    }
     return (celsius * 9) / 5 + 32;
   }
 }
