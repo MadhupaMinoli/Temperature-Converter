952ebbe728fe44db65ea032e44c3873c01ef35b1
Madhupa Minoli <mmadhupa@gmail.com>
2025-12-29 22:55:10 +0100

    Added Stryker Mutator | Dashboard

--- stryker.conf.mjs
+++ stryker.conf.mjs
@@ -4,17 +4,21 @@ const config = {
   _comment:
     "This config was generated using 'stryker init'. Please take a look at: https://stryker-mutator.io/docs/stryker-js/configuration/ for more information.",
   packageManager: "npm",
-  reporters: ["html", "clear-text", "progress"],
+  reporters: ["html", "clear-text", "progress", "dashboard"],
   testRunner: "jest",
   testRunner_comment:
     "Take a look at https://stryker-mutator.io/docs/stryker-js/jest-runner for information about the jest plugin.",
   coverageAnalysis: "perTest",
- mutate: ["src/**/*.js"],
+  mutate: ["src/**/*.js"],
   thresholds: {
     "high": 80,
     "low": 60,
     "break": 70
   },
   incremental: true,
+  dashboard: {
+    project: "MadhupaMinoli/Temperature-Converter", // your project name
+    version: "1.0.0",                              // version of this run
+  }
 };
 export default config;
