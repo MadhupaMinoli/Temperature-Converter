c8b9faa3d303cb567de8145ac9585038ba122864
Madhupa Minoli <mmadhupa@gmail.com>
2025-12-29 23:52:00 +0100

    Code Exclusion Feature

--- src/converterFahrenheit.js
+++ src/converterFahrenheit.js
@@ -1,9 +1,11 @@
 class ConverterFahrenheit {
   convert(celsius) {
     
+     // Stryker disable all
     if (celsius === null || celsius === undefined) {
       throw new Error('Invalid input');
     }
+    // Stryker restore all
     return (celsius * 9) / 5 + 32;
   }
 }
